b044f9a87860822ea7243a95b840211b29ba4e44
Kevin Bocksrocker <bokevin@student.ethz.ch>
2015-07-02 21:40:48 +0200

    Complete rewrite of the OpenAddressingTable implementation
    The old implementation contained a few significant (and rather obvious)
    race conditions.

--- util/OpenAddressingHash.cpp
+++ util/OpenAddressingHash.cpp
@@ -1,5 +1,6 @@
 #include "OpenAddressingHash.hpp"
 
+#include "helper.hpp"
 #include "Logging.hpp"
 
 #include <boost/functional/hash.hpp>
@@ -8,9 +9,6 @@ namespace tell {
 namespace store {
 
 namespace {
-
-void* const  gDeletedPtr = reinterpret_cast<void*>(0x1u);
-void* const gInvalidPtr = reinterpret_cast<void*>(0x2u);
 
 /**
  * @brief Check the wellformedness of tableId and key
@@ -24,8 +22,7 @@ void checkDataKey(uint64_t table, uint64_t /* key */) {
  */
 void checkDataPtr(const void* ptr) {
     LOG_ASSERT(ptr != nullptr, "Data pointer not allowed to be null");
-    LOG_ASSERT(ptr != gDeletedPtr, "The two LSBs have to be zero");
-    LOG_ASSERT(ptr != gInvalidPtr, "The two LSBs have to be zero");
+    LOG_ASSERT((reinterpret_cast<uintptr_t>(ptr) % 8 == 0), "Pointer not 8 byte aligned");
 }
 
 } // anonymous namespace
@@ -49,7 +46,7 @@ const void* OpenAddressingTable::get(uint64_t table, uint64_t key) const {
     });
 }
 
-bool OpenAddressingTable::insert(uint64_t table, uint64_t key, void* data) {
+bool OpenAddressingTable::insert(uint64_t table, uint64_t key, void* data, void** actualData /* = nullptr */) {
     checkDataKey(table, key);
     checkDataPtr(data);
 
@@ -58,42 +55,39 @@ bool OpenAddressingTable::insert(uint64_t table, uint64_t key, void* data) {
         auto& entry = mBuckets[pos % mCapacity];
         auto ptr = entry.ptr.load();
 
-        // Check if pointer is invalid
-        if (ptr == gInvalidPtr) {
+        // Check if pointer is already acquired (i.e. not a deletion and not free) - Skip bucket if it is
+        // There is no need to check if the insertion will conflict as this will be resolved at a later time.
+        if ((ptr != (to_underlying(EntryMarker::DELETED)) && (ptr != to_underlying(EntryMarker::FREE)))) {
             continue;
         }
 
-        // Check if pointer denotes a valid element
-        if (ptr != nullptr && ptr != gDeletedPtr) {
-            // Check if bucket belongs to another element, skip bucket if it does
-            if (entry.tableId.load() != table || entry.keyId.load() != key) {
-                continue;
-            }
-
-            // Entry exists
-            return false;
-        }
-
-        // Try to claim this bucket by setting the table
+        // Try to claim this bucket by setting the pointer to inserting
         // If this fails somebody else claimed the bucket in the mean time
-        uint64_t t = 0x0u;
-        if (!entry.tableId.compare_exchange_strong(t, table)) {
+        auto insertPtr = (reinterpret_cast<uintptr_t>(data) | to_underlying(EntryMarker::INSERTING));
+        if (!entry.ptr.compare_exchange_strong(ptr, insertPtr)) {
             continue;
         }
-        entry.keyId.store(key);
 
-        // Check if another concurrent insert conflicts with ours
-        // If this fails somebody else is inserting the same key, set pointer to invalid and free bucket
-        if (hasInsertConflict(hash, pos, table, key)) {
-            entry.ptr.store(gInvalidPtr);
+        // Write the key and table information
+        // Table has to be written last as it is read last by readers to detect if key was written correctly
+        entry.keyId.store(key);
+        entry.tableId.store(table);
+
+        // Check if another insert conflicts with ours
+        // If this fails somebody else is inserting or has already inserted the same key, set pointer to invalid and
+        // free bucket.
+        if (hasInsertConflict(hash, pos, table, key, actualData)) {
+            entry.ptr.store(to_underlying(EntryMarker::INVALID));
             deleteEntry(entry);
             return false;
         }
 
         // Try to set the pointer from null or deleted to our data pointer
-        // If this fails somebody else set us to invalid, so we have to free bucket
-        if (!entry.ptr.compare_exchange_strong(ptr, data)) {
+        // If this fails somebody else set the element to invalid and the bucket has to be released
+        if (!entry.ptr.compare_exchange_strong(insertPtr, reinterpret_cast<uintptr_t>(data))) {
             deleteEntry(entry);
+
+            if (actualData) *actualData = nullptr;
             return false;
         }
 
@@ -101,39 +95,80 @@ bool OpenAddressingTable::insert(uint64_t table, uint64_t key, void* data) {
     }
 }
 
-bool OpenAddressingTable::update(uint64_t table, uint64_t key, const void* oldData, void* newData) {
+bool OpenAddressingTable::update(uint64_t table, uint64_t key, const void* oldData, void* newData,
+        void** actualData /* = nullptr */) {
     checkDataKey(table, key);
     checkDataPtr(oldData);
     checkDataPtr(newData);
 
-    return execOnElement(table, key, false, [oldData, newData] (Entry& entry, const void* /* ptr */) {
-        // Set the element pointer from old to new
-        auto expected = oldData;
-        return entry.ptr.compare_exchange_strong(expected, newData);
+    return execOnElement(table, key, false, [table, key, oldData, newData, actualData] (Entry& entry, void* /* ptr */) {
+        auto expected = reinterpret_cast<uintptr_t>(oldData);
+        if (entry.ptr.compare_exchange_strong(expected, reinterpret_cast<uintptr_t>(newData))) {
+            return true;
+        }
+
+        setActualData(table, key, actualData, entry, expected);
+        return false;
     });
 }
 
-bool OpenAddressingTable::erase(uint64_t table, uint64_t key, const void* oldData) {
+bool OpenAddressingTable::erase(uint64_t table, uint64_t key, const void* oldData, void** actualData /* = nullptr */) {
     checkDataKey(table, key);
     checkDataPtr(oldData);
 
-    return execOnElement(table, key, true, [oldData] (Entry& entry, const void* /* ptr */) {
-        // Try to set the pointer to invalid
-        // If this fails then somebody else modified the element
-        auto expected = oldData;
-        if (!entry.ptr.compare_exchange_strong(expected, gInvalidPtr)) {
-            return false;
+    return execOnElement(table, key, true, [table, key, oldData, actualData] (Entry& entry, const void* /* ptr */) {
+        auto expected = reinterpret_cast<uintptr_t>(oldData);
+        if (entry.ptr.compare_exchange_strong(expected, to_underlying(EntryMarker::INVALID))) {
+            deleteEntry(entry);
+            return true;
         }
-        deleteEntry(entry);
 
-        return true;
+        setActualData(table, key, actualData, entry, expected);
+        return false;
     });
 }
 
 void OpenAddressingTable::deleteEntry(Entry& entry) {
     entry.tableId.store(0x0u);
     entry.keyId.store(0x0u);
-    entry.ptr.store(gDeletedPtr);
+    entry.ptr.store(to_underlying(EntryMarker::DELETED));
+}
+
+void OpenAddressingTable::setActualData(uint64_t table, uint64_t key, void** actualData, const Entry& entry,
+        uintptr_t ptr) {
+    if (!actualData) {
+        return;
+    }
+
+    while (true) {
+        // Check if pointer is marked as deleted, inserting or invalid (it will never be free)
+        if ((ptr & MARKER_MASK) != 0x0u) {
+            *actualData = nullptr;
+            return;
+        }
+
+        auto t = entry.tableId.load();
+        auto k = entry.keyId.load();
+
+        // Check if the pointer changed - Reload variables if it has
+        // Needed to ensure the loaded ptr, table and key are consistent and were not changed by other threads in the
+        // meantime. This does not suffer from the ABA problem as the table requires unique pointers and access through
+        // an epoch.
+        auto p = entry.ptr.load();
+        if (p != ptr) {
+            ptr = p;
+            continue;
+        }
+
+        // Check if the table and key of the bucket changed
+        if (t != table || k != key) {
+            *actualData = nullptr;
+            return;
+        }
+
+        *actualData = reinterpret_cast<void*>(ptr);
+        return;
+    }
 }
 
 uint64_t OpenAddressingTable::calculateHash(uint64_t table, uint64_t key) const {
@@ -142,87 +177,137 @@ uint64_t OpenAddressingTable::calculateHash(uint64_t table, uint64_t key) const 
     return hash;
 }
 
-bool OpenAddressingTable::hasInsertConflict(size_t hash, size_t insertPos, uint64_t table, uint64_t key) {
-    for (auto pos = hash;; ++pos) {
+bool OpenAddressingTable::hasInsertConflict(size_t hash, size_t insertPos, uint64_t table, uint64_t key,
+        void** actualData) {
+    auto pos = hash;
+    while (true) {
         // Skip our own entry
         if (pos == insertPos) {
+            ++pos;
             continue;
         }
 
         auto& entry = mBuckets[pos % mCapacity];
         auto ptr = entry.ptr.load();
 
-        // Check if pointer is invalid, skip bucket if it is
-        if (ptr == gInvalidPtr) {
-            continue;
-        }
-
-        auto t = entry.tableId.load();
-
-        // Check if we reached the end of the overflow bucket and nobody is writing to this bucket
-        if (ptr == nullptr && t == 0x0u) {
+        // Check if we reached the end of the overflow bucket
+        if (ptr == to_underlying(EntryMarker::FREE)) {
             LOG_ASSERT(pos > insertPos, "Reached end of overflow before reaching insert element");
             return false;
         }
 
-        // Check if bucket belongs to another element, skip bucket if it does
-        if (t != table || entry.keyId.load() != key) {
+        // Check if pointer is invalid or deleted - Skip bucket if it is
+        if ((ptr & (to_underlying(EntryMarker::DELETED) | to_underlying(EntryMarker::INVALID))) != 0x0u) {
+            ++pos;
             continue;
         }
 
-        // Check if the pointer is already set (not null without marker)
-        // In this case we have to abort the insert, because another thread already completed the insert
-        if ((reinterpret_cast<uintptr_t>(ptr) >> 2) != 0x0u) {
-            return true;
+        // At this point element is either inserting or valid
+        auto t = entry.tableId.load();
+        auto k = entry.keyId.load();
+
+        // Check if another thread is still writing data to the bucket - Skip bucket if it is
+        // The other insert thread has not yet entered the conflict detection scan, if the insert conflicts with this
+        // insert then the other thread will detect it as the current thread already wrote the table, key and pointer
+        // fields. A table ID of 0 means either deleted or beginning of insert or beginning of deletion as such this
+        // check can be made before we check the consistency in the next step.
+        if (t == 0x0u) {
+            ++pos;
+            continue;
         }
 
-        // At this point there is a concurrent insert conflict but both threads did not yet complete the insert
-        // We conflict if the element of the other thread is located in a bucket before our own insert bucket
-        if (pos < insertPos) {
+        // Check if the pointer changed - Reload variables if it has
+        // Needed to ensure the loaded ptr, table and key are consistent and were not changed by other threads in the
+        // meantime. This does not suffer from the ABA problem as the table requires unique pointers and access through
+        // an epoch.
+        if (entry.ptr.load() != ptr) {
+            continue;
+        }
+
+        // Check if bucket belongs to another element, skip bucket if it does
+        if (t != table || k != key) {
+            ++pos;
+            continue;
+        }
+
+        // Check whether the pointer is already set (i.e. has no inserting marker) or marks a concurrent insert and the
+        // element of the other thread is located in a bucket before our own insert bucket
+        if ((ptr & to_underlying(EntryMarker::INSERTING)) == 0x0u) {
+            // No concurrent insert, no deletion, no invalid, no free element as such the pointer has to be valid
+            // Abort as another thread already completed the insert
+            if (actualData) *actualData = reinterpret_cast<void*>(ptr);
+            return true;
+        } else if (pos < insertPos) {
+            // Concurrent insert and the element of the other thread is located in a bucket before our own insert bucket
+            // Abort our own insert
+            if (actualData) *actualData = nullptr;
             return true;
         }
 
         // The element is located in a bucket after our own insert bucket so we try to set the other pointer to invalid
-        // If this fails then the other thread completed its insert operation in the meantime and we have to abort
-        if (!entry.ptr.compare_exchange_strong(ptr, gInvalidPtr)) {
-            if (ptr != gInvalidPtr) {
-                return true;
-            }
+        // If this fails then the other thread might have completed its insert operation and we have to abort. This does
+        // not suffer from the ABA problem: The pointer points to the element with the INSERTING marker set, as the
+        // table accesses elements only through the epoch the element's memory will only be reused after this thread
+        // exits.
+        auto expected = ptr;
+        if (entry.ptr.compare_exchange_strong(expected, to_underlying(EntryMarker::INVALID))) {
+            ++pos;
+            continue;
         }
+
+        // Check if pointer is invalid or deleted, skip bucket if it is
+        if ((expected & (to_underlying(EntryMarker::DELETED) | to_underlying(EntryMarker::INVALID))) != 0x0u) {
+            ++pos;
+            continue;
+        }
+
+        // Check if the pointer is already set (not null without marker)
+        if ((ptr & POINTER_MASK) == expected) {
+            if (actualData) *actualData = reinterpret_cast<void*>(expected);
+            return true;
+        }
+
+        // The pointer changed completely - Recheck the current bucket
     }
 }
 
 template <typename T, typename F>
 T OpenAddressingTable::execOnElement(uint64_t table, uint64_t key, T notFound, F fun) const {
-    for (auto pos = calculateHash(table, key);; ++pos) {
+    auto pos = calculateHash(table, key);
+    while (true) {
         auto& entry = mBuckets[pos % mCapacity];
         auto ptr = entry.ptr.load();
 
-        // Check if pointer is invalid or deleted, skip bucket if it is
-        if ((reinterpret_cast<uintptr_t>(ptr) & (0x2u | 0x1u)) != 0) {
+        // Check if pointer is marked as deleted, inserting or invalid - Skip bucket if it is
+        if ((ptr & MARKER_MASK) != to_underlying(EntryMarker::FREE)) {
+            ++pos;
             continue;
         }
 
-        auto t = entry.tableId.load();
-
-        // Check if we reached the end of the overflow bucket
-        if (ptr == nullptr) {
-            // Check if somebody else is currently writing to this bucket, skip bucket if this is the case
-            if (t != 0x0u) {
-                continue;
-            }
-
-            // End of overflow bucket, element not found
+        // Check if this bucket marks the end of the overflow bucket - Return not-found if it is
+        if (ptr == to_underlying(EntryMarker::FREE)) {
             return notFound;
         }
 
+        auto t = entry.tableId.load();
+        auto k = entry.keyId.load();
+
+        // Check if the pointer changed - Reload variables if it has
+        // Needed to ensure the loaded ptr, table and key are consistent and were not changed by other threads in the
+        // meantime. This does not suffer from the ABA problem as the table requires unique pointers and access through
+        // an epoch.
+        if (entry.ptr.load() != ptr) {
+            continue;
+        }
+
         // Check if bucket belongs to another element, skip bucket if it does
-        if (t != table || entry.keyId.load() != key) {
+        if (t != table || k != key) {
+            ++pos;
             continue;
         }
 
         // Element found
-        return fun(entry, ptr);
+        return fun(entry, reinterpret_cast<const void*>(ptr));
     }
 }
 
@@ -230,7 +315,7 @@ template <typename T, typename F>
 T OpenAddressingTable::execOnElement(uint64_t table, uint64_t key, T notFound, F fun) {
     return const_cast<const OpenAddressingTable*>(this)->execOnElement(table, key, notFound,
             [&fun] (const Entry& entry, const void* ptr) {
-        return fun(const_cast<Entry&>(entry), ptr);
+        return fun(const_cast<Entry&>(entry), const_cast<void*>(ptr));
     });
 }
 
--- util/OpenAddressingHash.hpp
+++ util/OpenAddressingHash.hpp
@@ -9,9 +9,16 @@ namespace tell {
 namespace store {
 
 /**
- * @brief Lock-Free Open-Addressing hash table for associating a pointer with a <tableId, keyId> pair
+ * @brief Lock-Free Open-Addressing hash table for associating a pointer with a <table, key> pair
  *
- * Pointers must be 8 byte aligned and neither the tableId nor the keyId are allowed to be 0.
+ * The hash table is designed to store pointers to a record for a given table and key and as such has a few limitations:
+ *
+ * * A table ID of 0 is not allowed
+ * * Pointers have to be unique, i.e. two different <table, key> pairs are not allowed to map to the same pointer
+ * * Pointers must be 8 byte aligned
+ * * Interactions with the table must be made while in an active epoch which manages all pointers
+ *
+ * These limitations are required to prevent certain ABA problems from arising.
  */
 class OpenAddressingTable {
 public:
@@ -45,9 +52,10 @@ public:
      * @param table The table ID of the entry
      * @param key The key ID of the entry
      * @param data The new data pointer
+     * @param actualData Pointer to the element which caused the conflict
      * @return True if the insert was successful, false if the element already exists in the hash table
      */
-    bool insert(uint64_t table, uint64_t key, void* data);
+    bool insert(uint64_t table, uint64_t key, void* data, void** actualData = nullptr);
 
     /**
      * @brief Tries to update an already existing element from the old pointer to the new pointer
@@ -58,9 +66,10 @@ public:
      * @param key The key ID of the entry
      * @param oldData The previous data pointer
      * @param newData The new data pointer
+     * @param actualData Pointer to the element which caused the conflict
      * @return True if the update was successful, false if the element did not exist or the data pointer has changed
      */
-    bool update(uint64_t table, uint64_t key, const void* oldData, void* newData);
+    bool update(uint64_t table, uint64_t key, const void* oldData, void* newData, void** actualData = nullptr);
 
     /**
      * @brief Erases an already existing element from the hash table
@@ -68,9 +77,10 @@ public:
      * @param table The table ID of the entry
      * @param key The key ID of the entry
      * @param oldData The previous data pointer
+     * @param actualData Pointer to the element which caused the conflict
      * @return True if the element did not exist or was successfully removed, false if the data pointer has changed
      */
-    bool erase(uint64_t table, uint64_t key, const void* oldData);
+    bool erase(uint64_t table, uint64_t key, const void* oldData, void** actualData = nullptr);
 
 private:
     /**
@@ -80,18 +90,54 @@ private:
         Entry()
                 : tableId(0x0u),
                   keyId(0x0u),
-                  ptr(nullptr) {
+                  ptr(0x0u) {
         }
 
         std::atomic<uint64_t> tableId;
         std::atomic<uint64_t> keyId;
-        std::atomic<const void*> ptr;
+        std::atomic<uintptr_t> ptr;
     };
+
+    /**
+     * @brief The potential states a Entry pointer can be tagged with
+     */
+    enum class EntryMarker : uintptr_t {
+        /// The entry is still free
+        FREE = 0x0u,
+
+        /// The entry is deleted and can be reused (actual pointer will be null)
+        DELETED = 0x1u,
+
+        /// The entry is invalid and must not be touched (actual pointer will be null)
+        INVALID = (0x1u << 1),
+
+        /// Data is currently written to the entry
+        INSERTING = (0x1u << 2),
+    };
+
+    /**
+     * @brief Bitmask to retrieve the pure pointer from a marked Entry pointer
+     */
+    static constexpr uintptr_t POINTER_MASK = (UINTPTR_MAX << 3);
+
+    /**
+     * @brief Bitmask to retrieve the pure markers from a marked Entry pointer
+     */
+    static constexpr uintptr_t MARKER_MASK = ~POINTER_MASK;
 
     /**
      * @brief Helper function to erase the entries data and set the entry to deleted
      */
     static void deleteEntry(Entry& entry);
+
+    /**
+     * @brief Helper function setting the actual data pointer when the modification of the given entry failed
+     *
+     * @param actualData The pointer where to store the actual data
+     * @param entry Entry that the modification failed on
+     * @param ptr The actual pointer of the entry
+     */
+    static void setActualData(uint64_t table, uint64_t key, void** actualData, const Entry& entry, uintptr_t ptr);
 
     /**
      * @brief Calculates a 64 bit hash from the table and key ID
@@ -115,9 +161,10 @@ private:
      * @param pos The position the current element will be inserted
      * @param table The table ID of the entry
      * @param key The key ID of the entry
+     * @param actualData Pointer to the element which caused the conflict
      * @return Whether other inserts conflict with the current one
      */
-    bool hasInsertConflict(size_t hash, size_t pos, uint64_t table, uint64_t key);
+    bool hasInsertConflict(size_t hash, size_t pos, uint64_t table, uint64_t key, void** actualData);
 
     /**
      * @brief Searches the hash table for the element with table and key and executes the function on the element
