804f36270db75d16b6c2d482ea944aaf63a0656b
braunl <braunl@inf.ethz.ch>
2015-08-05 16:51:26 +0200

    fixed the capacity vs count issue in column map page design

--- deltamain/ColumnMapRecord.cpp
+++ deltamain/ColumnMapRecord.cpp
@@ -4,27 +4,29 @@
  * The memory layout of a column-map MV-DMRecord depends on the memory layout of a
  * column map page which is layed out the following way:
  *
- * - count: int32 to store the number of records that are stored in this page.
- *   We also define count^ which is: count^ = 2*((count+1)/2)
- * - GC-flag (4 byte): a non-zero value indicates that the page is currently
- *   being constructed by gc and hence variable-sized values have to be
- *   retrieved from a different page.
- * - key-version column: an array of size count of 16-byte values in format:
+ * - capacity: int32 to store the number of records that whould actually fit
+ *   into the columns part of the storage. However, as we do not know in advance
+ *   how much space the var-sized values will need, it might be that the actual
+ *   count of records is smaller than the capacity.
+ * - count: int32 to store the number of records that are actually stored in
+ *   this page.
+ * - key-version column: an array of size capcity of 16-byte values in format:
  *   |key (8 byte)|version (8 byte)|
- * - newest-pointers: an array of size count of 8-byte pointers to newest
- *   versions of records in the logs (as their is only one newest ptr per MV record
- *   and not per record version, only the first newestptr of every MV record is valid)
- * - null-bitmatrix: a bitmatrix of size count x (|Columns|+7)/8 bytes
- * - var-size-meta-data column: an array of size count of signed 4-byte values
+ * - newest-pointers: an array of size capacity of 8-byte pointers to newest
+ *   versions of records in the logs (as their is only one newest ptr per MV
+ *   record and not per record version, only the first newestptr of every MV
+ *   record is valid)
+ * - null-bitmatrix: a bitmatrix of size capacity x (|Columns|+7)/8 bytes
+ * - var-size-meta-data column: an array of size capacity of signed 4-byte values
  *   indicating the total size of all var-sized values of each record. This is
  *   used to allocate enough space for a record on a get request.
  *   MOREOVER: We set this size to zero to denote a version of a deleted tuple
  *   and to a negative number if the tuple is marked as reverted and will be
  *   deleted at the next GC phase. In that case, the absolute denotes the size.
  * - fixed-sized data columns: for each column there is an array of size
- *   count^ x value-size (4 or 8 bytes, as defined in schema)
+ *   capacity  x value-size (4 or 8 bytes, as defined in schema)
  * - var-sized data columns: for each colum there is an array of
- *   count x 8 bytes in format:
+ *   capacity  x 8 bytes in format:
  *   |4-byte-offset from page start into var-sized heap|4-byte prefix of value|
  * - var-sized heap: values referred from var-sized columns in the format
  *   |4-byte size (including the size field)|value|
@@ -35,7 +37,7 @@
  * 2) in order to get the correct address.
  *
  * MV records are stored as single records in such a way that they are clustered
- * together and ordered by version DESC (which makes get operations simpler).
+ * together and ordered by version DESC (which facilitates get-operations).
  */
 
 //the following includes are just for convenience when coding, not actually needed
@@ -55,12 +57,12 @@ public:
 
     /**
      * Given a reference to table, computes the beginning of the page (basePtr),
-     * the total number of records in this page (recordCount) and the index of the
+     * the total number of records in this page (capacity) and the index of the
      * current record within the page (return value).
      */
-    inline uint32_t getBaseKnowledge(const Table *table, const char *&basePtr, uint32_t &recordCount) const {
+    inline uint32_t getBaseKnowledge(const Table *table, const char *&basePtr, uint32_t &capacity) const {
         basePtr = table->pageManager()->getPageStart(mData);
-        recordCount = *(reinterpret_cast<const uint32_t*>(basePtr));
+        capacity = *(reinterpret_cast<const uint32_t*>(basePtr));
         return (reinterpret_cast<uint64_t>(mData-2-8-reinterpret_cast<uint64_t>(basePtr)) / 16);
     }
 
@@ -68,14 +70,18 @@ public:
  * As we always use computeBaseKnowledge in the same way, we have a macro for that.
  */
 #define COMPUTE_BASE_KNOWLEDGE(table) const char *basePtr; \
-    uint32_t recordCount; \
+    uint32_t capacity; \
     uint32_t index; \
-    index = getBaseKnowledge(table, basePtr, recordCount);
+    index = getBaseKnowledge(table, basePtr, capacity);
 
     /**
      * The following convenience functions are used to get pointers to items of interest
      * within the colum-oriented page.
      */
+
+    inline const uint32_t getRecordCount(const char *basePtr) const {
+        return *(reinterpret_cast<uint32_t*>(basePtr + 4));
+    }
 
     inline const char *getKeyVersionPtrAt(const uint32_t index, const char * basePtr) const {
         return basePtr + 8 + (index*16);
@@ -89,8 +95,8 @@ public:
         return reinterpret_cast<const uint64_t*>(getKeyVersionPtrAt(index, basePtr) + 8);
     }
 
-    inline const char *getNewestPtrAt(const uint32_t index, const char * basePtr, const uint32_t recordCount) const {
-        return basePtr + 8 + (recordCount*16) + (index*8);
+    inline const char *getNewestPtrAt(const uint32_t index, const char * basePtr, const uint32_t capacity) const {
+        return basePtr + 8 + (capacity*16) + (index*8);
 
     }
 
@@ -101,21 +107,17 @@ public:
             (table->getNumberOfFixedSizedFields() + table->getNumberOfVarSizedFields() + 7) / 8;
     }
 
-    inline char *getNullBitMapAt(const uint32_t index, const char * basePtr, const uint32_t recordCount, const size_t nullBitMapSize) const {
-        return basePtr + 8 + (recordCount*24) + (index*nullBitMapSize);
+    inline char *getNullBitMapAt(const uint32_t index, const char * basePtr, const uint32_t capacity, const size_t nullBitMapSize) const {
+        return basePtr + 8 + (capacity*24) + (index*nullBitMapSize);
     }
 
-    inline const int32_t *getVarsizedLenghtAt(const uint32_t index, const char * basePtr, const uint32_t recordCount, const size_t nullBitMapSize) const {
-        return reinterpret_cast<const int32_t*>(basePtr + 8 + (recordCount*(24 + nullBitMapSize)) + (index*4));
+    inline const int32_t *getVarsizedLenghtAt(const uint32_t index, const char * basePtr, const uint32_t capacity, const size_t nullBitMapSize) const {
+        return reinterpret_cast<const int32_t*>(basePtr + 8 + (capacity*(24 + nullBitMapSize)) + (index*4));
     }
 
-    inline uint32_t getRecordCountHat(const uint32_t recordCount) const {
-        return ((recordCount + 1) / 2) * 2;
-    }
-
-    inline char *getColumnNAt(const Table *table, const uint32_t N, const uint32_t index, const char * basePtr, const uint32_t recordCount, const size_t nullBitMapSize) const {
+    inline char *getColumnNAt(const Table *table, const uint32_t N, const uint32_t index, const char * basePtr, const uint32_t capacity, const size_t nullBitMapSize) const {
         // end of var-sized = beginning of fixed-sized columns
-        char *res = const_cast<char *>(reinterpret_cast<const char *>(getVarsizedLenghtAt(recordCount, basePtr, recordCount, nullBitMapSize)));
+        char *res = const_cast<char *>(reinterpret_cast<const char *>(getVarsizedLenghtAt(capacity, basePtr, capacity, nullBitMapSize)));
         const uint32_t fixedSizedFields= table->getNumberOfFixedSizedFields();
         uint32_t offset = 0;
         if (N > fixedSizedFields)   // we deal with a var-sized field, but not the first of them
@@ -123,9 +125,9 @@ public:
         else                        // we deal with a fixed-sized or the first var-sized field
             offset = table->getFieldOffset(N);
         offset -= table->getFieldOffset(0); // subtract header part (which does not exist in column format)
-        res += getRecordCountHat(recordCount) * offset;
+        res += capacity * offset;
         if (N > fixedSizedFields)
-            res += recordCount * (N - fixedSizedFields);
+            res += capacity * (N - fixedSizedFields) * 8;
         res += index * table->getFieldSize(N);
         return res;
     }
@@ -137,7 +139,7 @@ public:
 /**
  * The following macro is used to chare the common code in getNewest and casNewest.
  */
-#define GET_NEWEST auto ptr = reinterpret_cast<std::atomic<uint64_t>*>(const_cast<char*>(getNewestPtrAt(index, basePtr, recordCount))); \
+#define GET_NEWEST auto ptr = reinterpret_cast<std::atomic<uint64_t>*>(const_cast<char*>(getNewestPtrAt(index, basePtr, capacity))); \
     auto p = ptr->load(); \
     while (ptr->load() % 2) { \
         ptr = reinterpret_cast<std::atomic<uint64_t>*>(p - 1); \
@@ -149,7 +151,7 @@ public:
      * VersionIterator to compile. Once we really want to make it useful
      * we have to force the caller to give non-null arguments!
      */
-    T getNewest(const Table *table = nullptr, const uint32_t index = 0, const char * basePtr = nullptr, const uint32_t recordCount = 0) const {
+    T getNewest(const Table *table = nullptr, const uint32_t index = 0, const char * basePtr = nullptr, const uint32_t capacity = 0) const {
         GET_NEWEST
         return reinterpret_cast<char*>(p);
     }
@@ -220,7 +222,7 @@ public:
         COMPUTE_BASE_KNOWLEDGE(table)
         const size_t nullBitMapSize = getNullBitMapSize(table);
 
-        auto newest = getNewest(table, index, basePtr, recordCount);
+        auto newest = getNewest(table, index, basePtr, capacity);
         if (newest) {
             DMRecordImplBase<T> rec(newest);
             bool b;
@@ -239,7 +241,7 @@ public:
 
         bool found = false;
         uint64_t *key = const_cast<uint64_t *>(getKeyAt(index, basePtr));
-        int32_t *varLength = const_cast<int32_t *>(getVarsizedLenghtAt(index, basePtr, recordCount, nullBitMapSize));
+        int32_t *varLength = const_cast<int32_t *>(getVarsizedLenghtAt(index, basePtr, capacity, nullBitMapSize));
         for (; ; index++, key += 2, varLength++) {
             if (*varLength < 0) continue;
             isValid = true;
@@ -265,7 +267,7 @@ public:
 
             auto fixedSizeFields = table->getNumberOfFixedSizedFields();
             uint32_t recordSize = table->getFieldOffset(table->getNumberOfFixedSizedFields())
-                    + *getVarsizedLenghtAt(index, basePtr, recordCount, getNullBitMapSize(table));
+                    + *getVarsizedLenghtAt(index, basePtr, capacity, getNullBitMapSize(table));
             std::unique_ptr<char[]> buf(new char[recordSize]);  //TODO: is this what it should look like? Am I using the right allocator?
             char *res = buf.get();
             // copy fixed-sized columns
@@ -273,13 +275,13 @@ public:
             char *dest = res;
             for (uint i = 0; i < fixedSizeFields; i++)
             {
-                src = const_cast<char*>(getColumnNAt(table, i, index, basePtr, recordCount, nullBitMapSize));
+                src = const_cast<char*>(getColumnNAt(table, i, index, basePtr, capacity, nullBitMapSize));
                 auto fieldSize = table->getFieldSize(i);
                 memcpy(dest, src, fieldSize);
                 dest += fieldSize;
             }
             // copy var-sized colums in a batch
-            src = const_cast<char *>(getColumnNAt(table, fixedSizeFields, index, basePtr, recordCount, nullBitMapSize));
+            src = const_cast<char *>(getColumnNAt(table, fixedSizeFields, index, basePtr, capacity, nullBitMapSize));
             src = const_cast<char *>(basePtr + *(reinterpret_cast<uint32_t *>(src)));   // pointer to first field in var-sized heap
             memcpy(dest, src, *varLength);
 
