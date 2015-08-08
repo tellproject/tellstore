f7facf3b8502014fe53d9b5c46448169a11a7e46
Kevin Bocksrocker <bokevin@student.ethz.ch>
2015-08-08 19:05:54 +0200

    Move UnsafeAtomic into util

--- logstructured/Record.hpp
+++ logstructured/Record.hpp
@@ -1,56 +1,16 @@
 #pragma once
 
+#include <util/UnsafeAtomic.hpp>
+
 #include <crossbow/logger.hpp>
 
 #include <atomic>
 #include <cstdint>
 #include <limits>
-#include <type_traits>
 
 namespace tell {
 namespace store {
 namespace logstructured {
-
-/**
- * @brief Unsafe 128 bit atomic
- *
- * Allows for atomic 128 bit compare and swap but loads and stores are implemented as two separate 64 bit operations.
- */
-template <typename T>
-class alignas(16) UnsafeAtomic {
-public:
-    static_assert(sizeof(T) == 16, "Only 16 byte types are supported");
-    static_assert(alignof(T) == 16, "Only 16 byte aligned types are supported");
-
-#if defined(__GNUC__) && __GNUC__ < 5
-#else
-    static_assert(std::is_trivially_copyable<T>::value, "Only trivially copyable types are supported");
-#endif
-
-    T unsafe_load() const noexcept {
-        T result;
-        __atomic_load(reinterpret_cast<const uint64_t*>(&mElement), reinterpret_cast<uint64_t*>(&result),
-                std::memory_order_seq_cst);
-        __atomic_load(reinterpret_cast<const uint64_t*>(&mElement) + 1, reinterpret_cast<uint64_t*>(&result) + 1,
-                std::memory_order_seq_cst);
-        return result;
-    }
-
-    void unsafe_store(T element) noexcept {
-        __atomic_store(reinterpret_cast<uint64_t*>(&mElement), reinterpret_cast<uint64_t*>(&element),
-                std::memory_order_seq_cst);
-        __atomic_store(reinterpret_cast<uint64_t*>(&mElement) + 1, reinterpret_cast<uint64_t*>(&element) + 1,
-                std::memory_order_seq_cst);
-    }
-
-    bool compare_exchange_strong(T& expected, T desired) noexcept {
-        return __atomic_compare_exchange(&mElement, &expected, &desired, false, std::memory_order_seq_cst,
-                std::memory_order_seq_cst);
-    }
-
-private:
-    T mElement;
-};
 
 /**
  * @brief Types of record entries written to the log
