d9a8d85c3ccc1b672b9e78b2af681839de13a995
Kevin Bocksrocker <bokevin@student.ethz.ch>
2015-10-12 11:12:00 +0200

    Fix false error message

--- logstructured/GcScanProcessor.cpp
+++ logstructured/GcScanProcessor.cpp
@@ -301,7 +301,7 @@ bool GcScanProcessor::replaceElement(ChainedVersionRecord* oldElement, ChainedVe
 void GcScanGarbageCollector::run(const std::vector<Table*>& tables, uint64_t /* minVersion */) {
     for (auto i : tables) {
         LOG_TRACE("Starting garbage collection on table %1%", i->id());
-        if (!mStorage.scan(i->id(), nullptr)) {
+        if (mStorage.scan(i->id(), nullptr)) {
             LOG_ERROR("Unable to start Garbage Collection scan");
             return;
         }
