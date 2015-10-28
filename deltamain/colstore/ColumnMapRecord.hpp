0f96e5bca51b92e69bf9385f7c4474591a441b06
Kevin Bocksrocker <bokevin@student.ethz.ch>
2015-10-28 10:55:10 +0100

    Remove old documentation

--- deltamain/colstore/ColumnMapRecord.hpp
+++ deltamain/colstore/ColumnMapRecord.hpp
@@ -44,46 +44,6 @@ class ColumnMapContext;
 class ColumnMapMainPage;
 
 /**
- * The memory layout of a column-map MV-DMRecord depends on the memory layout of a
- * column map page which is layed out the following way:
- *
- * - count: uint32 to store the number of records that are actually stored in
- *   this page. We also define:
- *   count^: count rounded up to the next multiple of 2. This helps with proper
- *   8-byte alignment of values.
- * - 4-byte padding
- * - key-version column: an array of size count of 16-byte values in format:
- *   |key (8 byte)|version (8 byte)|
- * - newest-pointers: an array of size count of 8-byte pointers to newest
- *   versions of records in the logs (as their is only one newest ptr per MV
- *   record and not per record version, only the first newestptr of every MV
- *   record is valid)
- * - null-bitmatrix: a bitmatrix of size capacity x (|Columns|+7)/8 bytes
- * - var-size-meta-data column: an array of size count^ of signed 4-byte values
- *   indicating the total size of all var-sized values of each record (rounded up
- *   to the next multiple of 4. This is used to allocate enough space for a
- *   record on a get request.
- *   MOREOVER: We set this size to zero to denote a version of a deleted tuple
- *   and to a negative number if the tuple is marked as reverted and will be
- *   deleted at the next GC phase. In that case, the absolute denotes the size.
- * - fixed-sized data columns: for each column there is an array of size
- *   count^ x value-size (4 or 8 bytes, as defined in schema)
- * - var-sized data columns: for each colum there is an array of
- *   count  x 8 bytes in format:
- *   |4-byte-offset from page start into var-sized heap|4-byte prefix of value|
- * - var-sized heap: values referred from var-sized columns in the format
- *   |4-byte size (including the size field)|value|
- *
- * Pointers into a page (e.g. from log) point to the first key/version entry in
- * the key-version column, but have the second lowest bit set to 1 (in order to
- * make clear it is a columnMap-MV record). This bit has to be unset (by subtracting
- * 2) in order to get the correct address.
- *
- * MV records are stored as single records in such a way that they are clustered
- * together and ordered by version DESC (which facilitates get-operations).
- */
-
-/**
  * @brief Struct storing information about a single element in the column map page
  */
 struct alignas(8) ColumnMapMainEntry {
